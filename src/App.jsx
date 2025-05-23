5a3600c5678d5b4f4778f2d9b77dd539934f4ede
rafliprasetyo23 <rafli23si@mahasiswa.pcr.ac.id>
2025-05-23 09:33:02 +0700

    menu pelanggan by rafli

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+
 import { Route } from "react-router-dom";
+import Pelanggan from "./pages/Pelanggan";
 
 function App(){
   return(
     <Routes>
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Dashboard/>}/>
+      <Route path="/pelanggan" element={<Pelanggan/>}/>
+
       </Route>
     </Routes>
   )
