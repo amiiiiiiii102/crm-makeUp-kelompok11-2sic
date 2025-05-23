5803b6ef3ab99374c7ba61006f0fe0d0aad812ca
amiiiiiiii102 <aminah23si@mahasiswa.pcr.ac.id>
2025-05-23 09:45:55 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import { Route } from "react-router-dom";
+import ProductManagement from "./pages/ProductManagement";
 
 function App(){
   return(
     <Routes>
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Dashboard/>}/>
-     
+      <Route path="/produk" element={<ProductManagement/>}/>
       </Route>
     </Routes>
   )
 }
 
-export default pp;
+export default App;
