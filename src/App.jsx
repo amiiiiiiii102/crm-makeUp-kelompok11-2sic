c0cc95d1abc60fc280a3c0347f4dac0405c2a8c6
aminah <aminah23si@mahasiswa.pcr.ac.id>
2025-06-17 17:42:12 +0700

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home"; // Halaman utama tanpa layout
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Pelanggan from "./pages/pelanggan/Pelanggan";
+import TambahPelanggan from "./pages/pelanggan/TambahPelanggan";
+import EditPelanggan from "./pages/pelanggan/EditPelanggan";
 import ProductManagement from "./pages/ProductManagement";
 import SalesManagement from "./pages/SalesManagement";
 import ProductForm from "./pages/ProductForm";
 import Pemesanan from "./pages/Pemesanan";
 import ChatPelanggan from "./pages/ChatPelanggan";
 import FAQ from "./pages/FAQ";
+import Artikel from "./pages/Artikel";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import TambahPelanggan from "./pages/pelanggan/TambahPelanggan";
-import EditPelanggan from "./pages/pelanggan/EditPelanggan";
-import Artikel from "./pages/Artikel";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Halaman publik (tanpa layout) */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <Routes>
+      {/* Halaman publik (tanpa layout) */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-        {/* Halaman dengan layout dashboard */}
-        <Route element={<MainLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/pelanggan" element={<Pelanggan />} />
-          <Route path="/editpelanggan/:pelanggan_id" element={<EditPelanggan />} />
-          <Route path="/tambahpelanggan" element={<TambahPelanggan />} />
-          <Route path="/produk" element={<ProductManagement />} />
-          <Route path="/penjualan" element={<SalesManagement />} />
-          <Route path="/productform" element={<ProductForm />} />
-          <Route path="/pemesanan" element={<Pemesanan />} />
-          <Route path="/chatpelanggan" element={<ChatPelanggan />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/artikel" element={<Artikel />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      {/* Halaman dengan layout dashboard */}
+      <Route element={<MainLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/pelanggan" element={<Pelanggan />} />
+        <Route path="/tambahpelanggan" element={<TambahPelanggan />} />
+        <Route path="/editpelanggan/:pelanggan_id" element={<EditPelanggan />} />
+        <Route path="/produk" element={<ProductManagement />} />
+        <Route path="/penjualan" element={<SalesManagement />} />
+        <Route path="/productform" element={<ProductForm />} />
+        <Route path="/pemesanan" element={<Pemesanan />} />
+        <Route path="/chatpelanggan" element={<ChatPelanggan />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/artikel" element={<Artikel />} />
+      </Route>
+    </Routes>
   );
 }
 
